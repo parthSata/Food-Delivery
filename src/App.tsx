6f14351d6c89f5a0b9093f91cd5e1f41a39c0144
Parth <parthsata1709@gmail.com>
2024-07-01 09:20:42 +0530

    Bug

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ import Verification from "./Components/Verification";
 import NotAuthorized from "./Components/NotAuthorized";
 import { AuthProvider } from "./Components/AuthContext";
 import ProtectedRoute from "./Components/ProtectedRoute";
-import RestaurantTypes from "./Seller/RestaurantTypes";
 
 function App() {
   return (
